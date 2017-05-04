d73241b4f21bcfd49d48e2fd91fb581b10eaff9e
zacksleo <zacksleo@gmail.com>
2017-05-04 16:38:34 +0800

    update ci

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var cssnano = require('cssnano');
+
+var dirs = {
+    public: 'public'
+};
+
+gulp.task('useref', function(){
+    return gulp.src('public/**/*.html')
+        .pipe($.useref({
+            searchPath: 'public'
+        }))
+        .pipe($.uniqueFiles())
+        .pipe($.if('*.css', $.postcss([
+            cssnano()
+        ])))
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.rev())
+        .pipe($.revReplace({
+            prefix: '/'
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['useref']);
